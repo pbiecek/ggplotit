0ec0f67135695236d443ceaaabe20b808873cdef
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-19 21:55:14 +0200

    +scales

--- R/table.R
+++ R/table.R
@@ -3,6 +3,7 @@
 #' Function \code{ggplotit.table} plots objects of the class table{base}
 #'
 #' @param x An object of class table
+#' @param type One of seq (sequential), div (diverging) or qual (qualitative)
 #' @param ... Other parameters
 #' @return An ggplot2 plot
 #' @examples
@@ -11,13 +12,17 @@
 #' ggplotit(as.table(children))
 #' }
 #' @export
+#' @import scales
 #' @import ggplot2
+#' @import RColorBrewer
 
-ggplotit.table <- function(x, ...) {
+ggplotit.table <- function(x, type = "seq", ...) {
   stopifnot(length(dim(x)) == 2)
 
   df <- data.frame(x)
   dfn <- colnames(df)
   ggplot(df, aes_string(x=dfn[1], fill=dfn[2], y=dfn[3])) +
-    geom_bar(stat="identity", position="fill")
+    geom_bar(stat="identity", position="fill") +
+    theme_bw() + scale_y_continuous(label=percent) +
+    scale_fill_brewer(type=type)
 }
