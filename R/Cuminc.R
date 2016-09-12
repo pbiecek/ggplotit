6fd3b63432d9f929ffb5c6ca13c345525e8d6f81
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-09-12 18:16:50 +0200

    full plot for competing risks

--- R/Cuminc.R
+++ R/Cuminc.R
@@ -17,9 +17,15 @@
 #' @import ggplot2
 
 ggplotit.Cuminc <- function(x, ...) {
+  obj <- attr(x, "survfit")
+  mat <- obj$cumhaz[dim(obj$cumhaz)[1],,]
+  tmp <- data.frame(time = obj$time, t(mat))
+  tmp_long <- tidyr::gather(tmp[,-ncol(tmp)], var, val, -time)
 
   # ggplot2
-  pl <- ggplot()
+  pl <- ggplot(tmp_long, aes(time, val, color=var)) +
+    geom_point() +
+    geom_step()
 
   pl
 }
